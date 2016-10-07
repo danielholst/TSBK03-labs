ac8ccf19b827a7ff73d522b538e423b8d928865f
DanielHolst <danielholst2@gmail.com>
2016-10-07 13:49:12 +0200

    flocking done lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -6,12 +6,12 @@
 // gcc lab4.c ../common/*.c ../common/Mac/MicroGlut.m -o lab4 -framework OpenGL -framework Cocoa SpriteLight.c -I../common/Mac -I../common
 
 #ifdef __APPLE__
-	#include <OpenGL/gl3.h>
-	#include "MicroGlut.h"
-	// uses framework Cocoa
+#include <OpenGL/gl3.h>
+#include "MicroGlut.h"
+// uses framework Cocoa
 #else
-	//#include <GL/gl.h>
-	#include "MicroGlut.h"
+//#include <GL/gl.h>
+#include "MicroGlut.h"
 #endif
 
 #include <stdlib.h>
@@ -21,9 +21,13 @@
 #include <math.h>
 #include "VectorUtils3.h"
 
-#define radius 100.0
-
 // Lägg till egna globaler här efter behov.
+float CohesionRadius = 200.0f;
+float SeperationRadius = 100.0f;
+float CohesionWeight = 0.02f;
+float SeperationWeight = 0.01f;
+float AlignmentWeight = 0.01f;
+float maxSpeed = 5.0f;
 
 // add two Fpoints
 FPoint addPoint(FPoint p1, FPoint p2)
@@ -31,16 +35,30 @@ FPoint addPoint(FPoint p1, FPoint p2)
     FPoint p;
     p.v = p1.v + p2.v;
     p.h = p1.h + p2.h;
-    
     return p;
 }
 
+// subtract two Fpoints
+FPoint subPoint(FPoint p1, FPoint p2)
+{
+    FPoint p;
+    p.v = p1.v - p2.v;
+    p.h = p1.h - p2.h;
+    return p;
+}
 FPoint divPoint(FPoint p1, float d)
 {
     FPoint p;
     p.h = p1.h / d;
     p.v = p1.v / d;
-    
+    return p;
+}
+
+FPoint multPoint(FPoint p1, float f)
+{
+    FPoint p;
+    p.h = p1.h * f;
+    p.v = p1.v * f;
     return p;
 }
 
@@ -50,10 +68,9 @@ float Length(FPoint p) {
 
 FPoint normalize(FPoint p) {
     float length = Length(p);
-    FPoint retVal;
-    retVal.h = p.h / length;
-    retVal.v = p.v / length;
-    return retVal;
+    p.h = p.h / length;
+    p.v = p.v / length;
+    return p;
 }
 
 // get distance between two FPoints
@@ -61,121 +78,201 @@ float getDistance(FPoint p1, FPoint p2)
 {
     float diffX = p2.h - p1.h;
     float diffY = p2.v - p1.v;
-    
     return sqrt(diffX*diffX + diffY*diffY);
 }
+
+FPoint clamp(FPoint p1, float max)
+{
+    if (p1.h > max)
+        p1.h = max;
+    if (p1.h < -max)
+        p1.h = -max;
+    if (p1.v > max)
+        p1.v = max;
+    if (p1.v < -max)
+        p1.v = -max;
+    return p1;
+}
+
 void SpriteBehavior() // Din kod!
 {
-// Lägg till din labbkod här. Det går bra att ändra var som helst i
-// koden i övrigt, men mycket kan samlas här. Du kan utgå från den
-// globala listroten, gSpriteRoot, för att kontrollera alla sprites
-// hastigheter och positioner, eller arbeta från egna globaler.
-    
-    int creaturesInRadius = 1;
+    // Lägg till din labbkod här. Det går bra att ändra var som helst i
+    // koden i övrigt, men mycket kan samlas här. Du kan utgå från den
+    // globala listroten, gSpriteRoot, för att kontrollera alla sprites
+    // hastigheter och positioner, eller arbeta från egna globaler.
+
+    int creaturesInCohesionRadius = 1;
+    int creaturesInSeperationRadius = 1;
 
     SpritePtr current = gSpriteRoot;
     SpritePtr other = gSpriteRoot;
-    
-    FPoint centerPoint = {0, 0};
-    
+
+    FPoint cohesionPoint = {0, 0};
+    FPoint seperationPoint = {0, 0};
+    FPoint alignmentPoint = {0, 0};
+
+    FPoint cohesionDir = {0, 0};
+    FPoint seperationDir = {0, 0};
+    FPoint alignmentDir = {0, 0};
+
+    FPoint diffSpeed = {0, 0};
+    FPoint seperationVec = {0, 0};
+
+    float distance = 0;
+
     while(current) {
-        centerPoint = current->position;
+
+        cohesionPoint = current->position;
+        cohesionDir = multPoint(cohesionDir, 0.0f);
+        seperationDir = multPoint(seperationDir, 0.0f);
+        alignmentDir = multPoint(alignmentDir, 0.0f);
+
         while(other) {
-            
+
             // ignore when pointing to the same creature
             if (current == other) {
                 other = other->next;
                 continue;
             }
-            // if next creature is within the radius
-            if (getDistance(current->position, other->position) < radius)
-            {
-                if (getDistance(current->position, other->position) > 10) {
-                    centerPoint = addPoint(centerPoint, other->position);
-                    creaturesInRadius++;
-                }
+            distance = getDistance(current->position, other->position);
+
+            // if creature is within the different CohesionRadius
+            if (distance < CohesionRadius) {
+
+                creaturesInCohesionRadius++;
+
+                // for cohesion
+                cohesionPoint = addPoint(cohesionPoint, other->position);
+
+                // for alignment
+                diffSpeed = addPoint(diffSpeed, normalize(subPoint(other->speed, current->speed)));
+            }
+            if (distance < SeperationRadius) {
+
+                // for seperation
+                seperationVec = multPoint(normalize(subPoint(current->position, other->position)), SeperationRadius-distance);
+                seperationPoint = addPoint(seperationPoint, seperationVec);
+                creaturesInSeperationRadius++;
             }
 
             other = other->next;
         }
-        
-        if (creaturesInRadius > 1) {
-            centerPoint = divPoint(centerPoint, creaturesInRadius);
-            
-            FPoint direction = {centerPoint.h - current->position.h,
-                                centerPoint.v - current->position.v};
 
-            direction = normalize(direction);
-            
-            
-            float length = Length(current->speed);
-            current->speed.h = direction.h * length;
-            current->speed.v = direction.v * length;
+        if (creaturesInSeperationRadius > 1) {
+
+            seperationPoint = divPoint(seperationPoint, creaturesInSeperationRadius);
+            seperationDir = multPoint(seperationPoint, SeperationWeight);
         }
+
+        if (creaturesInCohesionRadius > 1) {
+            cohesionPoint = divPoint(cohesionPoint, creaturesInCohesionRadius);
+            cohesionDir = normalize(subPoint(cohesionPoint, current->position));
+            cohesionDir = multPoint(cohesionDir, CohesionWeight);
+
+            alignmentDir = multPoint(diffSpeed, AlignmentWeight);
+
+        }
+
+        FPoint direction = addPoint(addPoint(cohesionDir, seperationDir), alignmentDir);
+
+        current->speed = addPoint(current->speed, direction);
+
+        current->speed = clamp(current->speed, maxSpeed);
+
         other = gSpriteRoot;
         current = current->next;
-        creaturesInRadius = 1;
+        creaturesInCohesionRadius = 1;
+        creaturesInSeperationRadius = 1;
     }
-    
 }
 
 // Drawing routine
 void Display()
 {
-	SpritePtr sp;
-	
-	glClearColor(0, 0, 0.2, 1);
-	glClear(GL_COLOR_BUFFER_BIT+GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_TEXTURE_2D);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	
-	DrawBackground();
-	
-	SpriteBehavior(); // Din kod!
-	
-// Loop though all sprites. (Several loops in real engine.)
-	sp = gSpriteRoot;
-	do
-	{
-		HandleSprite(sp); // Callback in a real engine
-		DrawSprite(sp);
-		sp = sp->next;
-	} while (sp != NULL);
-	
-	glutSwapBuffers();
+    SpritePtr sp;
+
+    glClearColor(0, 0, 0.2, 1);
+    glClear(GL_COLOR_BUFFER_BIT+GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    DrawBackground();
+
+    SpriteBehavior(); // Din kod!
+
+    // Loop though all sprites. (Several loops in real engine.)
+    sp = gSpriteRoot;
+    do
+    {
+        HandleSprite(sp); // Callback in a real engine
+        DrawSprite(sp);
+        sp = sp->next;
+    } while (sp != NULL);
+
+    glutSwapBuffers();
 }
 
 void Reshape(int h, int v)
 {
-	glViewport(0, 0, h, v);
-	gWidth = h;
-	gHeight = v;
+    glViewport(0, 0, h, v);
+    gWidth = h;
+    gHeight = v;
 }
 
 void Timer(int value)
 {
-	glutTimerFunc(20, Timer, 0);
-	glutPostRedisplay();
+    glutTimerFunc(20, Timer, 0);
+    glutPostRedisplay();
 }
 
-// Example of user controllable parameter
-float someValue = 0.0;
-
+// read key input to change variables in run time
 void Key(unsigned char key,
          __attribute__((unused)) int x,
          __attribute__((unused)) int y)
 {
   switch (key)
   {
-    case '+':
-    	someValue += 0.1;
-    	printf("someValue = %f\n", someValue);
-    	break;
-    case '-':
-    	someValue -= 0.1;
-    	printf("someValue = %f\n", someValue);
-    	break;
+    case '.':
+        CohesionWeight += 0.01;
+        printf("kCohesionWeight = %f\n", CohesionWeight);
+        break;
+    case ',':
+        CohesionWeight -= 0.01;
+        printf("kCohesionWeight = %f\n", CohesionWeight);
+        break;
+    case 'l':
+        SeperationWeight += 0.01;
+        printf("kAvoidanceWeight = %f\n", SeperationWeight);
+        break;
+    case 'k':
+        SeperationWeight -= 0.01;
+        printf("kAvoidanceWeight = %f\n", SeperationWeight);
+        break;
+    case 'o':
+        AlignmentWeight += 0.01;
+        printf("kAlignmentWeight = %f\n", AlignmentWeight);
+        break;
+    case 'i':
+        AlignmentWeight -= 0.01;
+        printf("kAlignmentWeight = %f\n", AlignmentWeight);
+        break;
+    case 'm':
+        CohesionRadius += 5;
+        printf("cohesian_distance = %f\n", CohesionRadius);
+        break;
+    case 'n':
+        CohesionRadius -= 5;
+        printf("cohesian_distance = %f\n", CohesionRadius);
+        break;
+    case 'j':
+        SeperationRadius += 5;
+        printf("separation_distance = %f\n", SeperationRadius);
+        break;
+    case 'h':
+        SeperationRadius -= 5;
+        printf("separation_distance = %f\n", SeperationRadius);
+        break;
     case 0x1b:
       exit(0);
   }
@@ -183,19 +280,19 @@ void Key(unsigned char key,
 
 void Init()
 {
-	TextureData *sheepFace, *blackFace, *dogFace, *foodFace;
-	
-	LoadTGATextureSimple("bilder/leaves.tga", &backgroundTexID); // Bakgrund
-	
-	sheepFace = GetFace("bilder/sheep.tga"); // Ett får
-	blackFace = GetFace("bilder/blackie.tga"); // Ett svart får
-	dogFace = GetFace("bilder/dog.tga"); // En hund
-	foodFace = GetFace("bilder/mat.tga"); // Mat
-	
-	NewSprite(sheepFace, 100, 200, 1, 1);
-	NewSprite(sheepFace, 200, 100, 1.5, -1);
-	NewSprite(sheepFace, 250, 200, -1, 1.5);
-    
+    TextureData *sheepFace, *blackFace, *dogFace, *foodFace;
+
+    LoadTGATextureSimple("bilder/leaves.tga", &backgroundTexID); // Bakgrund
+
+    sheepFace = GetFace("bilder/sheep.tga"); // Ett får
+    blackFace = GetFace("bilder/blackie.tga"); // Ett svart får
+    dogFace = GetFace("bilder/dog.tga"); // En hund
+    foodFace = GetFace("bilder/mat.tga"); // Mat
+
+    NewSprite(sheepFace, 100, 200, 1, 1);
+    NewSprite(sheepFace, 200, 100, 1.5, -1);
+    NewSprite(sheepFace, 250, 200, -1, 1.5);
+
     NewSprite(sheepFace, 100, 300, 0.5, 1);
     NewSprite(sheepFace, 200, 400, -1, 1);
     NewSprite(sheepFace, 240, 50, 1, -1);
@@ -204,20 +301,20 @@ void Init()
 
 int main(int argc, char **argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutInitWindowSize(800, 600);
-	glutInitContextVersion(3, 2);
-	glutCreateWindow("SpriteLight demo / Flocking");
-	
-	glutDisplayFunc(Display);
-	glutTimerFunc(20, Timer, 0); // Should match the screen synch
-	glutReshapeFunc(Reshape);
-	glutKeyboardFunc(Key);
-	
-	InitSpriteLight();
-	Init();
-	
-	glutMainLoop();
-	return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitWindowSize(800, 600);
+    glutInitContextVersion(3, 2);
+    glutCreateWindow("SpriteLight demo / Flocking");
+
+    glutDisplayFunc(Display);
+    glutTimerFunc(20, Timer, 0); // Should match the screen synch
+    glutReshapeFunc(Reshape);
+    glutKeyboardFunc(Key);
+
+    InitSpriteLight();
+    Init();
+
+    glutMainLoop();
+    return 0;
 }
